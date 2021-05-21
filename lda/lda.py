74650b2d25a748b662cf744ff3d08bb2ad2932c3
yangej <yangej0218@gmail.com>
2021-05-21 08:33:52 +0800

    finish rough lda

--- lda/lda.py
+++ lda/lda.py
@@ -1,31 +1,36 @@
 #coding=utf-8
 
-# #匯入模組與資料並進行檢視
-# import pandas as pd
-# reviews_data = pd.read_csv('clean_data.csv')
-# reviews_data.head()
-
+from sklearn.feature_extraction.text import CountVectorizer
+from gensim import corpora, models, similarities
+import gensim
 import jieba
 import jieba.analyse
 import re
+import os
+import csv
+import pandas as pd
+from itertools import chain
 
 ## get stop words
-common_words = ['奧斯卡', '電影', '片中']
+common_words = ['奧斯卡', '電影', '片中', '最佳', '今年', '頒獎']
 with open('stop_word.txt') as f:
     stop_words = [line.strip() for line in f.readlines()]
 removed_words = common_words + stop_words
 f.close()
 
-print(removed_words)
-
 ## get dataset
+initial_data_tuples = []
 data_tuples = []
-with open('test_articles.csv') as f:
+with open('clean_data.csv') as f:
     title_and_content = ''
-    for str in f.readlines():
-        str_arr = str.split(',')
-        title_and_content = str_arr[3] + str_arr[4]
-        data_tuples.append((str_arr[0], title_and_content, str_arr[5]))
+    for line in f.readlines():
+        str_arr = line.split(',')
+        title = str_arr[3]
+        content = str_arr[4]
+        publisher = str_arr[5]
+        title_and_content = title + content
+        data_tuples.append((str_arr[0], title_and_content, publisher))
+        initial_data_tuples.append((str_arr[0], title, content))
 f.close()
 
 ## group dataset by year and split into eng & chi two parts
@@ -35,45 +40,69 @@ eng_regex = r'([A-Z]+[a-z]*)'
 for tuple in data_tuples:
     year = tuple[0]
     title_and_content = tuple[1]
-    eng_words = re.findall(eng_regex, title_and_content)
-    chi_words = ''.join([word for word in jieba.lcut(re.sub(eng_regex, '', title_and_content)) if word not in removed_words])
+    words = ''.join([word for word in jieba.lcut(title_and_content.replace(" ", "")) if word not in removed_words])
+    # ^ remove stop words
 
     if group_by_year.get(year) == None:
         years.append(year)
-        group_by_year[year] = { 'eng_words': eng_words, 'chi_words': chi_words }
+        group_by_year[year] = [words]
     else:
-        group_by_year[year]['eng_words'] += eng_words
-        group_by_year[year]['chi_words'] += chi_words
-print(group_by_year)
+        group_by_year[year].append(words)
 
-# chi_words = ''.join([word for word in words if word not in stopWords])
-# print('--------------------------------- 原文：')
-# print(chi_words)
-#
-# keywords = jieba.analyse.extract_tags(chi_words)
-# print('--------------------------------- 關鍵字：')
-# print(keywords)
-#
-# seg_list = jieba.lcut(chi_words, cut_all=True)
-# print('--------------------------------- 斷詞後：')
-# print(seg_list)
+group_by_year_titles = {}
+for tuple in initial_data_tuples:
+    year = tuple[0]
+    title = tuple[1]
 
-# #使用scikit-learn進行向量轉換
-# #忽略在文章中佔了90%的文字(即去除高頻率字彙)
-# #文字至少出現在2篇文章中才進行向量轉換
-# from sklearn.feature_extraction.text import CountVectorizer
-# cv = CountVectorizer(max_df = 0.9, min_df =2, stop_words = 'english')
-# df = df.dropna
-# dtm = cv.fit_transform(df['related_data'])
-#
-# #使用LDA演算法
-# from sklearn.decomposition import LatentDirichletAllocation
-# LDA = LatentDirichletAllocation(n_components=5, random_state=42)
-# LDA.fit(dtm)
-# #n_components => 想分成幾群
-# #random_state => 設定成42
-#
-# #觀看結果
-# for i,topic in enumerate(LDA.components_):
-#     print(f"TOP 10 WORDS PER TOPIC #{i}")
-#     print([cv.get_feature_names()[index] for index in topic.argsort()[-10:]])
+    if group_by_year_titles.get(year) == None:
+        group_by_year_titles[year] = [title]
+    else:
+        group_by_year_titles[year].append(title)
+
+year_lda = []
+year_clusters = []
+num_topics = [5, 6, 7, 8, 9, 10]
+for year in years:
+    current_year_docs = group_by_year[year]
+    dictionary = corpora.Dictionary([jieba.lcut(doc) for doc in current_year_docs])
+    corpus = [dictionary.doc2bow(jieba.lcut(doc)) for doc in current_year_docs]
+
+    for num in num_topics:
+        lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num)
+        dirPath = os.path.abspath(os.getcwd()) + f'/results/{year}_results/{year}_{num}_cluster_results'
+        try:
+            os.makedirs(dirPath)
+        except Exception as ex:
+            print('directory exists')
+
+        topFile = f'{year}_{num}_cluster.csv'
+        topPath = os.path.join(dirPath, topFile)
+        topFileExist = False
+        if (os.path.exists(topPath)):
+            topFileExist = True
+        with open(topPath, 'w', encoding='utf-8') as outfile:
+            writer = csv.writer(outfile)
+            if (topFileExist == False):
+                fieldNames = ['topic', 'keywords']
+                writer.writerow(fieldNames)
+
+            for topic in lda.print_topics(num_topics=num, num_words=10):
+                writer.writerow(topic)
+
+        docFile = f'{year}_{num}_cluster_document.csv'
+        docPath = os.path.join(dirPath, docFile)
+        docFileExist = False
+        if (os.path.exists(docPath)):
+            docFileExist = True
+        with open(docPath, 'w', encoding='utf-8') as outfile:
+            writer = csv.writer(outfile)
+            if (docFileExist == False):
+                fieldNames = ['topic', 'probability',  'title']
+                writer.writerow(fieldNames)
+
+            for i in range(len(group_by_year_titles[year])):
+                topic_pro = lda.get_document_topics(corpus[i])
+                tuple = (topic_pro, group_by_year_titles[year][i])
+                writer.writerow(tuple)
+
+    f.close()
