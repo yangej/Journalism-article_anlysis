b5827693649cf6aeed20d742bbbd9feac480dbd5
yangej <yangej0218@gmail.com>
2021-05-31 20:00:48 +0800

    add keyword calculation programs

--- lda/calculate_taiwan.py
+++ lda/calculate_taiwan.py
@@ -10,8 +10,10 @@ from ckiptagger import WS
 ws = WS("./data")
 
 class OutputFile:
-    def __init__(self, count_tuples):
+    def __init__(self, keyword, count_tuples, year_count_tuples):
+        self.keyword = keyword
         self.count_tuples = count_tuples
+        self.year_count_tuples = year_count_tuples
 
         dir_path = ''
         if (dataset_name == 'test_articles.csv'):
@@ -23,15 +25,39 @@ class OutputFile:
     def create_count_file(self):
         file = f'award_keyword_count.csv'
         file_path = os.path.join(self.__dir_path, file)
+        file_exists = False
+
+        if (os.path.exists(file_path)):
+            file_exists = True
 
         with open(file_path, 'w', encoding='utf-8') as outfile:
             writer = csv.writer(outfile)
-            field_names = ['topic',  'title']
-            writer.writerow(field_names)
+
+            if file_exists == False:
+                field_names = ['topic',  'title']
+                writer.writerow(field_names)
 
             for tuple in self.count_tuples:
                 writer.writerow(tuple)
         outfile.close()
+
+    def create_year_count_file(self):
+            file = f'award_{keyword}_count_by_year.csv'
+            file_path = os.path.join(self.__dir_path, file)
+            file_exists = False
+
+            if (os.path.exists(file_path)):
+                file_exists = True
+
+            with open(file_path, 'a', encoding='utf-8') as outfile:
+                writer = csv.writer(outfile)
+                if file_exists == False:
+                    field_names = ['topic',  'title']
+                    writer.writerow(field_names)
+
+                for tuple in self.year_count_tuples:
+                    writer.writerow(tuple)
+            outfile.close()
 
     def create_files(self):
         try:
@@ -40,6 +66,7 @@ class OutputFile:
             print('directory exists')
 
         self.create_count_file()
+        self.create_year_count_file()
 
 ## get stop words
 with open('stop_word.txt') as f:
@@ -51,34 +78,35 @@ with open('stop_word_manual.txt') as f:
 f.close()
 
 ## get dataset
-dataset_name = 'test_articles.csv'
-years = []
+dataset_name = 'award_dataset.csv'
+year_list = []
 titles = []
 texts_collection = []
 with open(dataset_name) as f:
     title_and_content = ''
-    for line in f.readlines():
-        str_arr = line.split(',')
-
-        year = str_arr[0]
-        title = str_arr[3]
-        content = str_arr[4]
+    reader = csv.reader(f)
+    next(reader, None)
+    for line in reader:
+        year = line[0]
+        title = line[3]
+        content = line[4]
         title_and_content = title + content
 
+        year_list.append(year)
         titles.append(title)
         texts_collection.append(title_and_content)
-        years.append(year)
 f.close()
 
 ## doc word vectors
 ws_results = ws(texts_collection)
-keyword = '李安'
+keyword = '台灣'
 
 ## group dataset by year and split into eng & chi two parts
 group_by_year = {}
-eng_regex = r'([A-Z]+[a-z]*)'
+years = []
+
 for i in range(len(ws_results)):
-    year = years[i]
+    year = year_list[i]
     texts = ws_results[i]
 
     if group_by_year.get(year) == None:
@@ -87,42 +115,45 @@ for i in range(len(ws_results)):
     else:
         group_by_year[year].append(texts)
 
+
 ## calculate keyword count grouping by year
 find_dictionary_by_year = {}
+year_count_tuples = []
 for year in years:
     current_year_docs = group_by_year[year]
     for index in range(len(current_year_docs)):
         doc = current_year_docs[index]
         for word in doc:
             if word == keyword:
-                if find_dictionary_by_year.get(word) == None:
-                    item = { word: [index] }
-                    find_dictionary_by_year[year] = item
+                if find_dictionary_by_year.get(year) == None:
+                    find_dictionary_by_year[year] = [index]
                 else:
-                    find_dictionary_by_year[year][word].append(index)
+                    find_dictionary_by_year[year].append(index)
                 break
+        continue
+    doc_count = len(find_dictionary_by_year[year])
+    tuple = (year, doc_count)
+    year_count_tuples.append(tuple)
 
-print(find_dictionary_by_year)
+print(year_count_tuples)
+# calculate keyword count not grouping by year
 
-# ## calculate keyword count not grouping by year
-#
-# find_dictionary = {}
-# for index in range(len(ws_results)):
-#     word_seg  = ws_results[index]
-#     for word in word_seg:
-#         if word in find_keyword:
-#             if find_dictionary.get(word) == None:
-#                 find_dictionary[word] = [index]
-#             else:
-#                 find_dictionary[word].append(index)
-#             break
-#
-# count_tuples = []
-# for keyword in find_keyword:
-#     docs = find_dictionary[keyword]
-#     tuple = (keyword, len(docs))
-#
-#     count_tuples.append(tuple)
-#
-# output = OutputFile(count_tuples)
-# output.create_files()
+find_list = {}
+for index in range(len(ws_results)):
+    word_seg  = ws_results[index]
+    for word in word_seg:
+        if word == keyword:
+            if len(find_list) == 0:
+                find_list = [index]
+            else:
+                find_list.append(index)
+            break
+
+count_tuples = []
+tuple = (keyword, len(find_list))
+
+count_tuples.append(tuple)
+print(count_tuples)
+
+output = OutputFile(keyword, count_tuples, year_count_tuples)
+output.create_files()
