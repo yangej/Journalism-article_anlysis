a486605c061d68c28e2eb02187256a333bf95fe3
yangej <yangej0218@gmail.com>
2021-06-05 20:04:54 +0800

    create program for calculating co-occuring counts

--- lda/calculate_co-occurence.py
+++ lda/calculate_co-occurence.py
@@ -1,65 +1,107 @@
 #coding=utf-8
 
-from wordcloud import WordCloud, ImageColorGenerator
-from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import GridSearchCV
 import numpy as np
 import re
 import os
 import csv
 from ckiptagger import WS
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+import networkx as nx
+font = fm.FontProperties(fname='./NotoSansTC-Regular.otf')  # speicify font
 ws = WS("./data")
 
-# ## get stop words
-# removed_words = []
-# with open('stop_word.txt') as f:
-#     removed_words = [line.strip() for line in f.readlines()]
-# f.close()
-#
-# with open('stop_word_manual.txt') as f:
-#     [removed_words.append(line.strip()) for line in f.readlines()]
-# f.close()
-#
-# ## get dataset
-# dataset_name = 'clean_data.csv'
-# titles = []
-# texts_collection = []
-# original_dataset = []
-# with open(dataset_name) as f:
-#     title_and_content = ''
-#     for line in f.readlines():
-#         str_arr = line.split(',')
-#
-#         year = str_arr[0]
-#         month = str_arr[1]
-#         date = str_arr[2]
-#         title = str_arr[3]
-#         content = str_arr[4]
-#         publisher = str_arr[5].strip()
-#         original_data = (year, month, date, title, content, publisher)
-#         title_and_content = title + content
-#
-#         original_dataset.append(original_data)
-#         titles.append(title)
-#         texts_collection.append(title_and_content)
-# f.close()
-# collection_length = len(original_dataset)
-#
-# ## split words by ckip
-# ws_results = ws(texts_collection)
-# clean_texts_collection  = []
-# index = 0
-# for word_seg in ws_results:
-#     clean_texts_collection.append((titles[index], ' '.join([word for word in word_seg if word not in removed_words])))
-#     index += 1
+## get stop words
+removed_words = []
+with open('stop_word.txt') as f:
+    removed_words = [line.strip() for line in f.readlines()]
+f.close()
 
-samples = ['awesome unicorns are awesome','batman forever and ever','I love batman forever']
-bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), vocabulary = {'awesome unicorns':0, 'batman forever':1})
-co_occurrences = bigram_vectorizer.fit_transform(samples)
-print 'Printing sparse matrix:', co_occurrences
-print 'Printing dense matrix (cols are vocabulary keys 0-> "awesome unicorns", 1-> "batman forever")', co_occurrences.todense()
-sum_occ = np.sum(co_occurrences.todense(),axis=0)
-print 'Sum of word-word occurrences:', sum_occ
-print 'Pretty printig of co_occurrences count:', zip(bigram_vectorizer.get_feature_names(),np.array(sum_occ)[0].tolist())
+with open('stop_word_manual.txt') as f:
+    [removed_words.append(line.strip()) for line in f.readlines()]
+f.close()
+
+## get dataset
+dataset_name = 'clean_data.csv'
+datasets = ['test_articles.csv']
+# datasets = ['all_5_0_dataset', 'all_5_1_dataset', 'all_5_2_dataset', 'all_5_3_dataset', 'all_5_4_dataset']
+for dataset in datasets:
+    cluster_num = dataset[6]
+    texts_collection = []
+    with open(dataset_name) as f:
+        title_and_content = ''
+        for line in f.readlines():
+            str_arr = line.split(',')
+
+            title = str_arr[3]
+            content = str_arr[4]
+            title_and_content = title + content
+
+            texts_collection.append(title_and_content)
+    f.close()
+
+    ## split words by ckip
+    ws_results = ws(texts_collection)
+    clean_texts_collection  = []
+    for word_seg in ws_results:
+        clean_texts_collection.append(' '.join([word for word in word_seg if word not in removed_words]))
+
+    ngram_size = 2
+
+    vectorizer = CountVectorizer(ngram_range=(ngram_size,ngram_size))
+    vectorizer.fit(clean_texts_collection) # build ngram dictionary
+    ngram = vectorizer.transform(clean_texts_collection) # get ngram
+
+    ngram_counts = ngram.toarray()
+    ngram_features = vectorizer.get_feature_names()
+    feature_tuples = []
+    for feature in ngram_features:
+        words = feature.split(' ')
+        tuple = (words[0], words[1])
+        feature_tuples.append(tuple)
+
+    count_dict = {}
+    for doc in ngram_counts:
+        for index in range(len(feature_tuples)):
+            if doc[index] == 1:
+                if count_dict.get(feature_tuples[index]) == None:
+                    count_dict[feature_tuples[index]] = 1
+                else:
+                    count_dict[feature_tuples[index]] += 1
+    print(count_dict)
+
+    # Create network plot
+
+    G = nx.Graph()
+
+    # Create connections between nodes
+    for word_tuple, count in count_dict.items():
+        G.add_edge(word_tuple[0], word_tuple[1], weight=(count * 10))
+
+    pos = nx.circular_layout(G)
+    edges = G.edges()
+    weights = [G[u][v]['weight'] for u,v in edges]
+    fig, ax = plt.subplots(figsize=(10, 8))
+    pos = nx.spring_layout(G, k=2)
+
+    # Plot networks
+    nx.draw_networkx(G, pos,
+                     font_size=16,
+                     width=weights,
+                     edge_color='grey',
+                     node_color='purple',
+                     with_labels = False,
+                     ax=ax)
+
+    # Create offset labels
+    for key, value in pos.items():
+        x, y = value[0]+.135, value[1]+.045
+        ax.text(x, y,
+            font_family=font,
+            s=key,
+            bbox=dict(facecolor='red', alpha=0.25),
+            horizontalalignment='center', fontsize=13)
+
+    plt.savefig(f'{cluster_num}_word_network.png')
+    print('finish!')
