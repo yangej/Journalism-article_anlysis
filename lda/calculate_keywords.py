1dc192d3f2451f98814c0cac7aeacb5f75f87eb7
yangej <yangej0218@gmail.com>
2021-06-05 20:03:27 +0800

    rename program for calculating keywords from certain dataset

--- lda/calculate_keywords.py
+++ lda/calculate_keywords.py
@@ -4,6 +4,7 @@ import re
 import os
 import csv
 from ckiptagger import WS
+from sklearn.feature_extraction.text import CountVectorizer
 ws = WS("./data")
 
 ## get stop words
@@ -71,7 +72,7 @@ def get_keyword_count(keyword_count, ws_results):
                      keyword_count[word] += 1
 
 def creat_keyword_count(topic_num, tuples):
-    filename = f'{topic_num}_keyword_count.csv'
+    filename = f'all_keyword_count.csv'
     output_path = os.path.join(os.path.abspath(os.getcwd()), filename)
     with open(output_path, 'w', encoding='utf-8') as outfile:
         writer = csv.writer(outfile)
@@ -84,35 +85,30 @@ def creat_keyword_count(topic_num, tuples):
 
 dataset = []
 texts_collection = []
-file_paths = ['./all_5_0_dataset.csv', './all_5_1_dataset.csv', './all_5_2_dataset.csv', './all_5_3_dataset.csv', './all_5_4_dataset.csv']
-keywords = [
-    ['導演','李安','影片','美國','入圍','主角','台灣','pi','藝術家','少年','法國','典禮','演員','夫人','票房','好萊塢','拿下','伊朗','大獎','美元'],
-    ['禮服','女星','搭配','紅毯','耳環','珠寶','lv','鑽石','時尚','復古','影后','項鍊','造型','現身','訂製','黑色','服裝','訂製服','金色','蕭邦'],
-    ['默片','烏吉','柯達','米歇爾阿札納維休斯','拍賣','精神獎','驚魂記','卡麥蓉狄亞','黛咪','希區考克','李奧納多','有聲','底片','比利懷德','主人','梅里葉','戰馬','凱特溫絲蕾','巴黎','the'],
-    ['典禮','梅莉史翠普','柴契爾','主角','影帝','影后','鐵娘子','演員','藝術家','女星','尚杜賈丹','英國','入圍','默片','演出','姊妹','角色','明星','派對','杜賈丹'],
-    ['失智症','醫師','中風','克里斯多夫','政治','走出','退化性','總統','照顧','病人','大學','英國','黑人','整形','病情','戰爭','透過','血管性','徐文俊','肉毒桿菌素']
-]
+file_paths = ['./clean_data.csv']
+# file_paths = ['./all_5_0_dataset.csv', './all_5_1_dataset.csv', './all_5_2_dataset.csv', './all_5_3_dataset.csv', './all_5_4_dataset.csv']
 for file_index in range(len(file_paths)):
     file_path = file_paths[file_index]
     topic_num = file_path[8]
-    keyword = keywords[file_index]
 
     ## get needed materials
     get_texts_and_original_data(file_path, texts_collection, dataset)
 
     ## split words by ckip
     ws_results = ws(texts_collection)
+    clean_texts_collection  = []
+    for word_seg in ws_results:
+        clean_texts_collection.append(' '.join([word for word in word_seg if word not in removed_words]))
 
     ## get keyword count results
-    keyword_count = {}
-    get_keyword_count(keyword_count, ws_results)
-    keyword_count_tuples = list(keyword_count.items())
-
-    ## get keyword count for each document
-    docs_results = []
-    get_keyword_count_for_documents(docs_results, ws_results)
+    cv = CountVectorizer()
+    cv_fit=cv.fit_transform(clean_texts_collection)
+    word_list = cv.get_feature_names()
+    word_counts = cv_fit.toarray().sum(axis=0)
+    keyword_counts = dict(zip(word_list,word_counts))
+    keyword_count_tuples = list(keyword_counts.items())
 
     ## create result files
-    creat_dataset_with_count(topic_num, dataset)
+#     creat_dataset_with_count(topic_num, dataset)
     creat_keyword_count(topic_num, keyword_count_tuples)
 
